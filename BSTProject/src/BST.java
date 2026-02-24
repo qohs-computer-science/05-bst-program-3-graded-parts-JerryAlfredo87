a998e278741788810d0a76f1337e8872c08ee0f2
Preston Havenstein <meadehavenstein@gmail.com>
2026-02-24 16:07:57 +0000

    still working - 2/24

--- BSTProject/src/BST.java
+++ BSTProject/src/BST.java
@@ -2,5 +2,57 @@ import java.lang.Comparable;
 
 public class BST implements BSTInterface
 {
+    TreeNode root;
+    public BST()
+    {
+        root = new TreeNode(null);
+    }
+    public void add(Comparable val)
+    {
+        if (root != null)
+        {
+            addHelper(val, root);
+        }
+        else
+        {
+            root = new TreeNode(val);
+        }
+    }
+    public void addHelper(Comparable val, TreeNode subroot)
+    {
+        if(val.compareTo(subroot) <= 0)
+        {
+            if(subroot.getLeft() != null)
+            {
+                addHelper(val, subroot.getLeft());
+            }
+            else
+            {
+                subroot.setLeft(new TreeNode(val));
+            }
+        }
+        else
+        {
+            if(subroot.getRight() != null)
+            {
+                addHelper(val, subroot.getRight());
+            }
+            else
+            {
+                subroot.setRight(new TreeNode(val));
+            }
+        }
+    }
+    /*public void printInOrder()
+    {
 
+    }
+    public void printPreOrder()
+    {
+        
+    }
+    public void printPostOrder()
+    {
+        
+    }*/
 }
--- BSTProject/src/BSTTester.java
+++ BSTProject/src/BSTTester.java
@@ -1,14 +1,15 @@
 /*
- * TODO: Name
- * TODO: Date
- * TODO: Class Period
- * TODO: Program Description
+ * Name: Meade Havenstein
+ * Date: 2/24/26
+ * Class Period: 4
+ * TODO: Program Description: 
  */
 
  import java.lang.Comparable;
 
  public class BSTTester {
-   public static void main(String[] args) {
+   public static void main(String[] args)
+   {
    
-	
+   }
  }
